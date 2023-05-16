4e3bb7cf5bd88587121e2d197063cc30cd2f462f
AngelV02 <125764846+AngelV02@users.noreply.github.com>
2023-05-16 15:16:30 -0700

    Add project files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+
+var musicVideo = document.getElementById("music-video");
+
+function animateVideo() {
+    musicVideo.style.transform = "rotate(360deg)";
+    musicVideo.style.transition = "transform 2s ease-in-out";
+}
+
+// Call the animateVideo function after the page has loaded
+window.onload = function () {
+    animateVideo();
+};
